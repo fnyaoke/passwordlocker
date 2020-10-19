f2801c2b341e6478158b0b1b37bdcf1e50e685de
fnyaoke <fnyaoke@gmail.com>
2020-10-19 10:57:36 +0300

    added function to create users

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3.6
 from usercredentials import users, Credentials
+
+def create_user(firstname,secondname,username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = users(firstname,secondname,username,password)
+    return new_user
