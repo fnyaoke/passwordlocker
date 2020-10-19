36f88c558a19c59bd50fbd998185e7ee9679e9c4
fnyaoke <fnyaoke@gmail.com>
2020-10-19 10:50:44 +0300

    added class to test the credentials

--- credentialstest.py
+++ credentialstest.py
@@ -26,3 +26,64 @@ class TestUser(unittest.TestCase):
         to undo the tests before this
         '''
         users.list_user=[]
+
+class  TestCredentials (unittest.TestCase):
+
+    def setUp(self):
+        self.new_credential = Credentials('RoseFlower','Ar0se@2020','Sweetrose')
+
+    def test__init(self):
+        self.assertEqual(self.new_credential.username,'RoseFlower')
+        self.assertEqual(self.new_credential.password,'Ar0se@2020')
+        self.assertEqual(self.new_credential.account_name,'Sweetrose')
+    def test_user_exists(self):
+        self.new_user = users('Florence','Nyaoke','flonyaks','p@ssw0rd1')
+        self.new_user.store_user()
+
+        for user in users.list_user:
+            if(user.username==self.new_user.username and user.password == self.new_user.password):
+                userlogin = user.username
+            return userlogin
+
+    def test_save_credentials(self):
+        '''
+        test if programme can save credentials
+        '''
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credentials.account_credentials),1)
+
+    def test_generate_userpassword(self):
+        generate_password = Credentials.generate_password(self,10)
+        self.assertTrue(generate_password)
+
+    def test_display_credentials(self):
+
+        self.assertEqual(Credentials.display_credentials(),Credentials.account_credentials)
+
+    def tearDown(self):
+        Credentials.account_credentials = []
+    def test_findbyaccount(self):
+        '''
+        test if credentials can be found using their account name
+        '''
+        self.new_credential.save_credential()
+        newTestcredential = Credentials('Doe','does','facebook')
+        newTestcredential.save_credential()
+        foundcredential = Credentials.find_by_account('facebook')
+        self.assertEqual(foundcredential,newTestcredential)
+
+    def test_removecredential(self):
+        '''
+        test if credential can be removed
+        '''
+        self.new_credential.save_credential()
+        newTestcredential = Credentials('Doe','does','facebook')
+        newTestcredential.save_credential()
+        newTestcredential.remove_credential()
+        self.assertEqual(len(Credentials.account_credentials),1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
