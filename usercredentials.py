199b8e183786bfdf6370f7f1941c0ca3ee29c1cf
fnyaoke <fnyaoke@gmail.com>
2020-10-19 08:49:28 +0300

    created class to store user information

--- usercredentials.py
+++ usercredentials.py
@@ -3,3 +3,23 @@ import random
 import string
 global list_user
 
+class users:
+    '''
+    class to create users and save their information
+    '''
+
+    list_user = []
+    def __init__(self, firstname, secondname,username,password):
+        '''
+         method to initialize properties that each  user object will hold
+        '''
+        self.firstname = firstname
+        self.secondname = secondname
+        self.username = username
+        self.password = password
+    def store_user(self):
+        '''
+        method to save user
+        '''
+        users.list_user.append(self)
+
